805136c377b5a5d494b167f612a327aa11bf63f6
Markus Koechl <markus.koechl@autodesk.com>
2024-10-02 00:00:31 +0200

    evaluate separate Login dialog before addin start

---
+++ Autodesk.TS.InvPlmAddIn/Forms/FMExtensionLogin.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace InvPlmAddIn.Forms
+{
+    partial class FMExtensionLogin
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            simpleButton1 = new DevExpress.XtraEditors.SimpleButton();
+            simpleButton2 = new DevExpress.XtraEditors.SimpleButton();
+            panelControl1 = new DevExpress.XtraEditors.PanelControl();
+            ((System.ComponentModel.ISupportInitialize)panelControl1).BeginInit();
+            SuspendLayout();
+            // 
+            // simpleButton1
+            // 
+            simpleButton1.Anchor = System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right;
+            simpleButton1.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            simpleButton1.Location = new System.Drawing.Point(309, 407);
+            simpleButton1.Name = "simpleButton1";
+            simpleButton1.Size = new System.Drawing.Size(75, 23);
+            simpleButton1.TabIndex = 0;
+            simpleButton1.Text = "Cancel";
+            // 
+            // simpleButton2
+            // 
+            simpleButton2.Anchor = System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right;
+            simpleButton2.DialogResult = System.Windows.Forms.DialogResult.OK;
+            simpleButton2.Location = new System.Drawing.Point(390, 407);
+            simpleButton2.Name = "simpleButton2";
+            simpleButton2.Size = new System.Drawing.Size(75, 23);
+            simpleButton2.TabIndex = 1;
+            simpleButton2.Text = "Proceed";
+            // 
+            // panelControl1
+            // 
+            panelControl1.Anchor = System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right;
+            panelControl1.Location = new System.Drawing.Point(12, 12);
+            panelControl1.Name = "panelControl1";
+            panelControl1.Size = new System.Drawing.Size(453, 389);
+            panelControl1.TabIndex = 2;
+            // 
+            // FMExtensionLogin
+            // 
+            AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            ClientSize = new System.Drawing.Size(477, 442);
+            Controls.Add(panelControl1);
+            Controls.Add(simpleButton2);
+            Controls.Add(simpleButton1);
+            Name = "FMExtensionLogin";
+            Text = "FMExtensionLogin";
+            ((System.ComponentModel.ISupportInitialize)panelControl1).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private DevExpress.XtraEditors.SimpleButton simpleButton1;
+        private DevExpress.XtraEditors.SimpleButton simpleButton2;
+        private DevExpress.XtraEditors.PanelControl panelControl1;
+    }
+}
---
+++ Autodesk.TS.InvPlmAddIn/Forms/FMExtensionLogin.cs
@@ -0,0 +1,52 @@
+﻿using DevExpress.XtraEditors;
+using Microsoft.Web.WebView2.WinForms;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static DevExpress.XtraPrinting.Native.ExportOptionsPropertiesNames;
+using VDF = Autodesk.DataManagement.Client.Framework;
+
+namespace InvPlmAddIn.Forms
+{
+    public partial class FMExtensionLogin : DevExpress.XtraEditors.XtraForm
+    {
+
+
+        public FMExtensionLogin(string currentTheme)
+        {
+            InitializeComponent();
+
+            ApplyThemes(currentTheme);
+
+            // Set the webview handler
+            WebView2 webView = new WebView2();
+            this.panelControl1.Controls.Add(webView);
+            webView.Dock = DockStyle.Fill;
+
+            //InvPlmAddIn.Model.WebViewHandler mWebViewHandler = new global::InvPlmAddIn.Model.WebViewHandler("https://www.plm.tools:9600/addins/navigate", new Model.HostObject(this));
+                   
+        }
+
+        private void mWebViewInit()
+        {
+
+        }
+
+        private void ApplyThemes(string currentTheme)
+        {
+            if (string.IsNullOrEmpty(currentTheme))
+                return;
+
+            if (currentTheme == VDF.Forms.SkinUtils.Theme.Light.ToString())
+                LookAndFeel.SetSkinStyle(VDF.Forms.SkinUtils.CustomThemeSkins.LightThemeName);
+            else if (currentTheme == VDF.Forms.SkinUtils.Theme.Dark.ToString())
+                LookAndFeel.SetSkinStyle(VDF.Forms.SkinUtils.CustomThemeSkins.DarkThemeName);
+        }
+    }
+}
